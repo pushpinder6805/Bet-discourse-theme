445d6180e7dcdf1c19083efd164ff2dcd879a4c8
pinder99 <52063297+pushpinder6805@users.noreply.github.com>
2024-09-01 15:13:05 +0530

    script

---
+++ javascripts/discourse/components/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var hideButton = document.getElementById('hide');
+    if (hideButton) {
+        hideButton.addEventListener('click', hideIntro);
+    }
+});
